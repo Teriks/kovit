89df5fce004769de29fec38b46ddf5707374896e
Teriks <Teriks@users.noreply.github.com>
2018-04-21 18:21:40 -0500

    Bump revision

--- kovit/__init__.py
+++ kovit/__init__.py
@@ -23,7 +23,7 @@ from kovit.bag import ProbabilityBag, BagItem
 from kovit.chain import Chain
 
 
-__version__ = '0.1.0.0a1'
+__version__ = '0.1.0.1a1'
 
 __all__ = ['Chain',
            'ProbabilityBag',
