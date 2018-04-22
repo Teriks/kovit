7f0c20d6f8dbdbc88cb1edaab1f6dc2a8dcd1577
Teriks <Teriks@users.noreply.github.com>
2018-04-22 16:51:16 -0500

    Go back to simple sdist uploads with travis for now

--- kovit/__init__.py
+++ kovit/__init__.py
@@ -23,7 +23,7 @@ from kovit.bag import ProbabilityBag, BagItem
 from kovit.chain import Chain
 
 
-__version__ = '0.1.4.0a1'
+__version__ = '0.1.4.1a1'
 
 __all__ = ['Chain',
            'ProbabilityBag',
