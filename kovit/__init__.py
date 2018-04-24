af2c56820e2c5de82566040c40f67fb375849678
Teriks <Teriks@users.noreply.github.com>
2018-04-24 07:00:18 -0500

    Only deploy sdist on linux CI job

--- kovit/__init__.py
+++ kovit/__init__.py
@@ -23,7 +23,7 @@ from kovit.bag import ProbabilityBag, BagItem
 from kovit.chain import Chain
 
 
-__version__ = '0.1.15.1a1'
+__version__ = '0.1.16.0a1'
 
 __all__ = ['Chain',
            'ProbabilityBag',
