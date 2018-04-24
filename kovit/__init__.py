02f52623addb17a6751c6358e3485aaab499c76a
Teriks <Teriks@users.noreply.github.com>
2018-04-24 02:04:01 -0500

    Add appveyor config to build windows wheels

--- kovit/__init__.py
+++ kovit/__init__.py
@@ -23,7 +23,7 @@ from kovit.bag import ProbabilityBag, BagItem
 from kovit.chain import Chain
 
 
-__version__ = '0.1.5.0a1'
+__version__ = '0.1.6.0a1'
 
 __all__ = ['Chain',
            'ProbabilityBag',
