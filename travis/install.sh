ca7881fb18d76faa6b89e8ea24e6fee0fd574376
Teriks <Teriks@users.noreply.github.com>
2018-04-24 05:40:21 -0500

    debug mac wheels on travis

--- travis/install.sh
+++ travis/install.sh
@@ -3,5 +3,5 @@
 if [[ "$(uname -s)" == 'Darwin' ]]; then
     sw_vers
     curl -o python36.pkg https://www.python.org/ftp/python/3.6.5/python-3.6.5-macosx10.6.pkg
-    installer -pkg python36.pkg -target /
+    sudo installer -pkg python36.pkg -target /
 fi
