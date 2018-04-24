726361b522e2b6a8fc9f719357e9d278b891cc11
Teriks <Teriks@users.noreply.github.com>
2018-04-24 06:09:48 -0500

    debug mac wheels on travis

--- travis/codecov.sh
+++ travis/codecov.sh
@@ -6,4 +6,9 @@ if [[ "$(uname -s)" != 'Darwin' ]]; then
     else
         python3 -m codecov
     fi
+else
+    # test but don't upload coverage on mac
+    if [[ $1 == run_tests ]]; then
+        python3 run_tests.py
+    fi
 fi
