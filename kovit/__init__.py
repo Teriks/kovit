57b8aaef28fa862bafaba14004f56f5e3facb8f0
Teriks <Teriks@users.noreply.github.com>
2018-04-24 02:31:24 -0500

    Attempt to fix CIBW_SKIP on appveyor

--- kovit/__init__.py
+++ kovit/__init__.py
@@ -23,7 +23,7 @@ from kovit.bag import ProbabilityBag, BagItem
 from kovit.chain import Chain
 
 
-__version__ = '0.1.8.0a1'
+__version__ = '0.1.9.0a1'
 
 __all__ = ['Chain',
            'ProbabilityBag',
