41376a69ea8617cdd36c751b67e7f65822831afe
Teriks <Teriks@users.noreply.github.com>
2018-04-24 20:14:52 -0500

    attempt to run tests on built wheels

--- travis/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [[ "$(uname -s)" == 'Darwin' ]]; then
-    sw_vers
-    curl -o python36.pkg https://www.python.org/ftp/python/3.6.5/python-3.6.5-macosx10.6.pkg
-    sudo installer -verbose -pkg python36.pkg -target /
-fi
